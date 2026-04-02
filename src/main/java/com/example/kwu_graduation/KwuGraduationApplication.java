d579fe315fe198a585c8ee5b1c3b973688a878e2
taesungs <70329288+taesungs@users.noreply.github.com>
2026-04-02 19:26:04 +0900

    init

---
+++ src/main/java/com/example/kwu_graduation/KwuGraduationApplication.java
@@ -0,0 +1,13 @@
+package com.example.kwu_graduation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwuGraduationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KwuGraduationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kwu_graduation/KwuGraduationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kwu_graduation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwuGraduationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
