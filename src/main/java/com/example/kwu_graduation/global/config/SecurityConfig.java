4af40c657540d356d4c705d772452ef6584b150a
taesungs <70329288+taesungs@users.noreply.github.com>
2026-05-28 18:54:18 +0900

    feat: Bean 추가

--- src/main/java/com/example/kwu_graduation/global/config/SecurityConfig.java
+++ src/main/java/com/example/kwu_graduation/global/config/SecurityConfig.java
@@ -17,6 +17,7 @@ public class SecurityConfig {
     };
 
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
